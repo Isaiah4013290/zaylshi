b93421ad755c32dfca63f935ce0ee1590d12f406
Isaiah4013290 <w.isaiah.thompson@gmail.com>
2026-02-26 20:22:32 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css'
 import { Nav } from '@/components/Nav'
 
 export const metadata: Metadata = {
-  title: 'Pick\'em',
+  title: 'zPickem',
   description: 'Predict. Compete. Win.',
 }
 
